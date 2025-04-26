c25f053e9c6641f86d4602e5e27e91e72b2153f5
sourengit <nath.souren743@gmail.com>
2025-04-26 12:50:45 +0530

    third demo added

--- src/abstraction/Customer.java
+++ src/abstraction/Customer.java
@@ -22,6 +22,19 @@ public abstract class Customer {
 		System.out.println("git test success, method updated!!");
 	}
 	
+	public void testGit2()
+	{
+		System.out.println("git test success, method updated!!");
+	}
+	
+	
+	public void testGitDemo3()
+	{
+		System.out.println("by branchDemo3 method");
+		
+	}
+	
+	
 	public abstract void BookingServer();
 	
 	public abstract void BookingAgent();
